81f3aa4b970f4f73409fb6a320f575744548437f
Iván <iquinterom1600@alumno.ipn.mx>
2022-03-11 21:19:28 -0600

    Ascendant List Validations for Binary Search

--- Practica1/main.py
+++ Practica1/main.py
@@ -8,11 +8,22 @@ from read_file import parse_array
 file_path = 'test.txt'
 x = 9999
 
+
+# Validate List for Binary Search
+def is_valid_list(list: List[int]):
+    list_length = len(list)
+    for i in range(list_length):
+        if i != list_length - 1:
+            if list[i] > list[i + 1]:
+                return False
+    return True
+
+
 # Binary Search Algorithm
 def binary_search(list: List[int], target: int):
     l = 0
     r = len(list) - 1
-    while(l <= r):
+    while (l <= r):
         mid = l + (r - l) // 2
         if list[mid] == target:
             print(f'Elemento en el índice: {mid}')
@@ -23,6 +34,7 @@ def binary_search(list: List[int], target: int):
             r = mid - 1
     print("Elemento no encontrado")
     return
+
 
 # Linear Search Algorithm
 def linear_search(list: List[int], target: int):
@@ -36,32 +48,29 @@ def linear_search(list: List[int], target: int):
 
 # Times Flags
 cpu_start_time = 0
-real_start_time = 0
 cpu_binary_time = 0
-real_binary_time = 0
 cpu_linear_time = 0
-real_linear_time = 0
 
 # Run Binary and Linear Search
 list = parse_array(file_path)
 if list:
-    # Binary Search
-    print("Búsqueda Binaria")
-    cpu_start_time = time()
-    real_start_time = datetime.now()
-    binary_search(list, x)
-    cpu_binary_time = (time()-cpu_start_time)*1000
-    real_binary_time = datetime.now()-real_start_time
-    print(f'Milisegundos: {cpu_binary_time}')
+    if is_valid_list(list):
+        # Binary Search
+        print("Búsqueda Binaria")
+        cpu_start_time = time()
+        binary_search(list, x)
+        cpu_binary_time = (time() - cpu_start_time) * 1000
+        print(f'Milisegundos: {cpu_binary_time}')
+    else:
+        print("ALERTA: La búsqueda binaria requiere una lista ordenada ascendentemente")
+        cpu_binary_time = 0
     # Linear Search
-    print("Búsqueda Linear")
+    print("Búsqueda Lineal")
     cpu_start_time = time()
-    real_start_time = datetime.now()
     linear_search(list, x)
-    cpu_linear_time = (time()-cpu_start_time)*1000
-    real_linear_time = datetime.now()-real_start_time
+    cpu_linear_time = (time() - cpu_start_time) * 1000
     print(f'Milisegundos: {cpu_linear_time}')
-    plt.bar(["Binaria", "Linear"],[cpu_binary_time,cpu_linear_time])
+    plt.bar(["Binaria", "Lineal"], [cpu_binary_time, cpu_linear_time])
     plt.title("Comparación de tiempo en algoritmos de búsqueda")
     plt.xlabel("Tipo de búsqueda")
     plt.ylabel("Tiempo (Milisegundos)")
