877e9b427539697c4564506a52debcfc6e2b2173
ORTIZJIMENEZANTONIO <you@example.com>
2022-04-09 16:58:06 -0500

    third algorithm added

--- Practica4/main.py
+++ Practica4/main.py
@@ -1,5 +1,6 @@
 from typing import List
 import math
+import copy
 import os
 import sys
 sys.path.append( os.getcwd() + "/mixins")
@@ -14,9 +15,129 @@ target = 10
 number_x: int = 12
 number_y: int =  100
 
+# A class to represent a Point in 2D plane
+class Point():
+	def __init__(self, x, y):
+		self.x = x
+		self.y = y
+P = [Point(2, 3), Point(12, 30),
+  	Point(40, 50), Point(5, 1),
+  	Point(12, 10), Point(3, 4)]
+n = len(P)
+
 
 # Change Recursion maximum
 sys.setrecursionlimit(10000)
+
+
+# A utility function to find the
+# distance between two points
+def dist(p1, p2):
+	return math.sqrt((p1.x - p2.x) *
+					(p1.x - p2.x) +
+					(p1.y - p2.y) *
+					(p1.y - p2.y))
+
+# A Brute Force method to return the
+# smallest distance between two points
+# in P[] of size n
+def bruteForce(P, n):
+	min_val = float('inf')
+	for i in range(n):
+		for j in range(i + 1, n):
+			if dist(P[i], P[j]) < min_val:
+				min_val = dist(P[i], P[j])
+
+	return min_val
+
+# A utility function to find the
+# distance between the closest points of
+# strip of given size. All points in
+# strip[] are sorted according to
+# y coordinate. They all have an upper
+# bound on minimum distance as d.
+# Note that this method seems to be
+# a O(n^2) method, but it's a O(n)
+# method as the inner loop runs at most 6 times
+def stripClosest(strip, size, d):
+	# Initialize the minimum distance as d
+	min_val = d
+	
+	# Pick all points one by one and
+	# try the next points till the difference
+	# between y coordinates is smaller than d.
+	# This is a proven fact that this loop
+	# runs at most 6 times
+	for i in range(size):
+		j = i + 1
+		while j < size and (strip[j].y -
+							strip[i].y) < min_val:
+			min_val = dist(strip[i], strip[j])
+			j += 1
+
+	return min_val
+
+# A recursive function to find the
+# smallest distance. The array P contains
+# all points sorted according to x coordinate
+def closestUtil(P, Q, n):
+	
+	# If there are 2 or 3 points,
+	# then use brute force
+	if n <= 3:
+		return bruteForce(P, n)
+
+	# Find the middle point
+	mid = n // 2
+	midPoint = P[mid]
+
+	#keep a copy of left and right branch
+	Pl = P[:mid]
+	Pr = P[mid:]
+
+	# Consider the vertical line passing
+	# through the middle point calculate
+	# the smallest distance dl on left
+	# of middle point and dr on right side
+	dl = closestUtil(Pl, Q, mid)
+	dr = closestUtil(Pr, Q, n - mid)
+
+	# Find the smaller of two distances
+	d = min(dl, dr)
+
+	# Build an array strip[] that contains
+	# points close (closer than d)
+	# to the line passing through the middle point
+	stripP = []
+	stripQ = []
+	lr = Pl + Pr
+	for i in range(n):
+		if abs(lr[i].x - midPoint.x) < d:
+			stripP.append(lr[i])
+		if abs(Q[i].x - midPoint.x) < d:
+			stripQ.append(Q[i])
+
+	stripP.sort(key = lambda point: point.y) #<-- REQUIRED
+	min_a = min(d, stripClosest(stripP, len(stripP), d))
+	min_b = min(d, stripClosest(stripQ, len(stripQ), d))
+	
+	
+	# Find the self.closest points in strip.
+	# Return the minimum of d and self.closest
+	# distance is strip[]
+	return min(min_a,min_b)
+
+# The main function that finds
+# the smallest distance.
+# This method mainly uses closestUtil()
+def closest(P, n):
+	P.sort(key = lambda point: point.x)
+	Q = copy.deepcopy(P)
+	Q.sort(key = lambda point: point.y)
+
+	# Use recursive function closestUtil()
+	# to find the smallest distance
+	return closestUtil(P, Q, n)
 
 
 def multiply_by_karatsuba(num_x: int, num_y: int):
@@ -49,27 +170,21 @@ def pow(number:int, target:int):
 
 
 if __name__ == '__main__':
-  karatsuba_time = functions.algotithm_time( multiply_by_karatsuba(number_x, number_y), "Algoritmo de karatsuba" )
+  karatsuba_time = functions.algotithm_time( multiply_by_karatsuba(number_x, number_y), "Karatsuba" )
   print(f'Milisegundos: {karatsuba_time} \n ')
 
-  normal_time = functions.algotithm_time( number_x * number_y, "Algoritmo de multiply" )
+  normal_time = functions.algotithm_time( closest(P, n), "Menor distancia" )
   print(f'Milisegundos: {normal_time} \n')
   
-  divide_conquer_pow_time = functions.algotithm_time( pow(number, target), "Algoritmo de potencia" )
-  print(f'Milisegundos: {divide_conquer_pow_time} \n')
+  pow_time = functions.algotithm_time( pow(number, target), "Potencia" )
+  print(f'Milisegundos: {pow_time} \n')
+ 
+
 
   functions.graph_bars(
-    ['Karatsuba', 'Estandard'],
-    [ karatsuba_time, normal_time ],
+    ['Karatsuba', 'Menor distancia', "Potencia"],
+    [ karatsuba_time, normal_time, pow_time ],
     'Comparación de tiempo',
     'Algoritmo',
     'Tiempo (Milisegundos)'
     )
-
-  functions.graph_bars(
-    [ "Potencia" ],
-    [ divide_conquer_pow_time ],
-    "Comparación de tiempo en algoritmos de búsqueda",
-    "Tipo de ordenamiento",
-    "Tiempo (Milisegundos)"
-  )
