1cf0bf1607bda8ca937e1ad8d3a5205859bff3db
ORTIZJIMENEZANTONIO <you@example.com>
2022-04-09 17:06:56 -0500

    refact code

--- Practica4/main.py
+++ Practica4/main.py
@@ -30,84 +30,43 @@ n = len(P)
 sys.setrecursionlimit(10000)
 
 
-# A utility function to find the
-# distance between two points
-def dist(p1, p2):
-	return math.sqrt((p1.x - p2.x) *
-					(p1.x - p2.x) +
-					(p1.y - p2.y) *
-					(p1.y - p2.y))
-
-# A Brute Force method to return the
-# smallest distance between two points
-# in P[] of size n
 def bruteForce(P, n):
 	min_val = float('inf')
 	for i in range(n):
 		for j in range(i + 1, n):
-			if dist(P[i], P[j]) < min_val:
-				min_val = dist(P[i], P[j])
+			if functions.dist(P[i], P[j]) < min_val:
+				min_val = functions.dist(P[i], P[j])
 
 	return min_val
 
-# A utility function to find the
-# distance between the closest points of
-# strip of given size. All points in
-# strip[] are sorted according to
-# y coordinate. They all have an upper
-# bound on minimum distance as d.
-# Note that this method seems to be
-# a O(n^2) method, but it's a O(n)
-# method as the inner loop runs at most 6 times
+
 def stripClosest(strip, size, d):
-	# Initialize the minimum distance as d
 	min_val = d
-	
-	# Pick all points one by one and
-	# try the next points till the difference
-	# between y coordinates is smaller than d.
-	# This is a proven fact that this loop
-	# runs at most 6 times
 	for i in range(size):
 		j = i + 1
 		while j < size and (strip[j].y -
 							strip[i].y) < min_val:
-			min_val = dist(strip[i], strip[j])
+			min_val = functions.dist(strip[i], strip[j])
 			j += 1
 
 	return min_val
 
-# A recursive function to find the
-# smallest distance. The array P contains
-# all points sorted according to x coordinate
+
 def closestUtil(P, Q, n):
-	
-	# If there are 2 or 3 points,
-	# then use brute force
 	if n <= 3:
 		return bruteForce(P, n)
 
-	# Find the middle point
 	mid = n // 2
 	midPoint = P[mid]
 
-	#keep a copy of left and right branch
 	Pl = P[:mid]
 	Pr = P[mid:]
 
-	# Consider the vertical line passing
-	# through the middle point calculate
-	# the smallest distance dl on left
-	# of middle point and dr on right side
 	dl = closestUtil(Pl, Q, mid)
 	dr = closestUtil(Pr, Q, n - mid)
 
-	# Find the smaller of two distances
 	d = min(dl, dr)
 
-	# Build an array strip[] that contains
-	# points close (closer than d)
-	# to the line passing through the middle point
 	stripP = []
 	stripQ = []
 	lr = Pl + Pr
@@ -121,22 +80,14 @@ def closestUtil(P, Q, n):
 	min_a = min(d, stripClosest(stripP, len(stripP), d))
 	min_b = min(d, stripClosest(stripQ, len(stripQ), d))
 	
-	
-	# Find the self.closest points in strip.
-	# Return the minimum of d and self.closest
-	# distance is strip[]
 	return min(min_a,min_b)
 
-# The main function that finds
-# the smallest distance.
-# This method mainly uses closestUtil()
+
 def closest(P, n):
 	P.sort(key = lambda point: point.x)
 	Q = copy.deepcopy(P)
 	Q.sort(key = lambda point: point.y)
 
-	# Use recursive function closestUtil()
-	# to find the smallest distance
 	return closestUtil(P, Q, n)
 
 
@@ -173,7 +124,7 @@ if __name__ == '__main__':
   karatsuba_time = functions.algotithm_time( multiply_by_karatsuba(number_x, number_y), "Karatsuba" )
   print(f'Milisegundos: {karatsuba_time} \n ')
 
-  normal_time = functions.algotithm_time( closest(P, n), "Menor distancia" )
+  normal_time = functions.algotithm_time( closest(P, n), "Menor functions.distancia" )
   print(f'Milisegundos: {normal_time} \n')
   
   pow_time = functions.algotithm_time( pow(number, target), "Potencia" )
@@ -182,7 +133,7 @@ if __name__ == '__main__':
 
 
   functions.graph_bars(
-    ['Karatsuba', 'Menor distancia', "Potencia"],
+    ['Karatsuba', 'Menor functions.distancia', "Potencia"],
     [ karatsuba_time, normal_time, pow_time ],
     'Comparación de tiempo',
     'Algoritmo',
--- Practica4/mixins/functions.py
+++ Practica4/mixins/functions.py
@@ -1,6 +1,13 @@
 from typing import List
 from time import time
 import matplotlib.pyplot as plot
+import math
+
+def dist(p1, p2):
+  	return math.sqrt((p1.x - p2.x) *
+					(p1.x - p2.x) +
+					(p1.y - p2.y) *
+					(p1.y - p2.y))
 
 
 def number_length( num: int ):
@@ -15,11 +22,10 @@ def algotithm_time( algorithm, title: str ):
   return cpu_time_end_karatsuba
 
 
+# Graph
 def graph_bars(label: List[str], data: List[float], title: str, xlabel: str, ylabel: str):
   plot.bar(label,  data)
   plot.title(title)
   plot.xlabel(xlabel)
   plot.ylabel(ylabel)
   plot.show()
-
-
